d91fbfabf02731964afa18295f817b9fa3e19d75
ignaciogonzalez12x2 <76555566+ignaciogonzalez12x2@users.noreply.github.com>
2022-05-09 18:26:19 +0200

    Fixed problems

--- contracts/EHealth.sol
+++ contracts/EHealth.sol
@@ -1,29 +1,23 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity >=0.7.4;
 pragma abicoder v2;
-
-// Factory contract for eDelivery
+// Factory contract for eHealth
 contract EHealth {
-    uint nextIdDoctor;
     uint nextIdSensor;
     // Owner direction 
     address owner = 0x8FcF3B91599C6d5bb93D8c7df9f3974C622b83a4;
-
+    // Oracle direction 
+    address public oracleAddress = 0x4B0AF919E9D85ea5CeEbe21ee4F8E3b2A2951b15;
     // Hospitals
     mapping(address=> Hospital) public Hospitals;
-
     struct Hospital{
         address idHospital;
         string name;
-        string city;
         string state;
-        uint256 postalCode;
+        string postalCode;
         bool permission;
     }
-
     Hospital[] public hospitalCenters;
-    
     // Doctors
     mapping(address=> Doctor) public Doctors;
 
@@ -35,10 +29,8 @@ contract EHealth {
     }
     // Types of doctors
     Doctor[] public doctorsHospital;
-
     // Sensors
     mapping(address=> Sensor) public Sensors;
-    
     struct Sensor{
         uint idSensor;
         string MAC;
@@ -49,26 +41,31 @@ contract EHealth {
     }
     // Types of sensors
     Sensor[] public sensorsDoctor;
-
     // EVENTS 
-    event NewHospital(address,string,string,string,uint,bool);
-    event DeleteHospital(address,string,string,string,uint,bool);
-    event RefreshHospital(address,string,string,string,uint,bool);
+    event NewHospital(address,string,string,string,bool);
+    event DeleteHospital(bool);
+    event RefreshHospital(address,string,string,string,bool);
+    event readOneHospital(string , string , string , bool);
+    event getAllHospital();
     event NewDoctor(address, string, address, bool);
-    event DeleteDoctor(address, string, address, bool);
+    event DeleteDoctor(bool);
     event RefreshDoctor(address, string, address,bool);
+    event readOneDoctor(string, address, bool);
     event NewSensor(uint,string,string,address,bool, uint[]);
-    event DeleteSensor(uint,string,string,address,bool);
+    event DeleteSensor(bool);
     event RefreshSensor(uint,string,string,address,bool);
+    event readOneSensor (string ,string , address, bool);
+    event sendingData(uint[]);
+    event viewData(uint[]);
 
     // *********************************** HOSPITALS ***********************************
-
+    
     // Add hospital in the system
-    function AddHospital(address _idHospital, string memory _name, string memory _city, string memory _state, uint _postalCode ) public {
-        hospitalCenters.push(Hospital(_idHospital, _name, _city, _state, _postalCode, true));
-        emit NewHospital(_idHospital, _name, _city, _state, _postalCode, true);
+    function AddHospital(address _idHospital, string memory _name, string memory _state, string memory _postalCode ) 
+    public onlyOracle (oracleAddress,owner){
+        hospitalCenters.push(Hospital(_idHospital, _name, _state, _postalCode, true));
+        emit NewHospital(_idHospital, _name, _state, _postalCode, true);
     }
-
     function FindHospital(address _address) internal view returns(uint){
         for(uint i = 0 ; i < hospitalCenters.length ; i++){
             if(hospitalCenters[i].idHospital==_address){
@@ -77,65 +74,46 @@ contract EHealth {
         }
         revert('Hospital not found');
     }
-
     // Remove permission of the hospital
-    function RemoveHospital(address _address) public {
+    function RemoveHospital(address _address) 
+    public onlyOracle (oracleAddress,owner){
         uint index = FindHospital(_address);
         hospitalCenters[index].permission=false;
-        emit DeleteHospital(hospitalCenters[index].idHospital, 
-        hospitalCenters[index].name, 
-        hospitalCenters[index].city, 
-        hospitalCenters[index].state, 
-        hospitalCenters[index].postalCode, 
-        hospitalCenters[index].permission);
+        emit DeleteHospital(hospitalCenters[index].permission);
     }
-
     // Update hospital data
-
-    function UpdateHospital(address _address, string memory _name, string memory _city, string memory _state, uint _postalCode, bool _permission ) public {
+    function UpdateHospital(address _address, string memory _name, string memory _state, string memory _postalCode, bool _permission ) 
+    public onlyOracle (oracleAddress,owner){
         uint index = FindHospital(_address);
         hospitalCenters[index].name=_name;
-        hospitalCenters[index].city=_city;
         hospitalCenters[index].state=_state;
         hospitalCenters[index].postalCode=_postalCode;
         hospitalCenters[index].permission=_permission;
         emit RefreshHospital(hospitalCenters[index].idHospital, 
         hospitalCenters[index].name, 
-        hospitalCenters[index].city, 
         hospitalCenters[index].state, 
         hospitalCenters[index].postalCode,
         hospitalCenters[index].permission
         );
     }
-
     // All hospitals 
-
-    function getAllHospitals() public view returns (Hospital[] memory){
-        Hospital[] memory ret = new Hospital[](hospitalCenters.length);
-        for (uint i = 0; i< hospitalCenters.length; i++) {
-            ret[i]=hospitalCenters[i];
-        }
-        return ret;
+    function getAllHospitals() 
+    public onlyOracle (oracleAddress,owner) view returns (Hospital[] memory){
+                return hospitalCenters;
     }
-
-    // Number of hospitals
-
-    function Length_Hospitals() public view returns (uint256){
-        return hospitalCenters.length;
-    }
-
     // Read hospital data
-
-    function ReadHospital(address _address)public view returns(string memory, string memory, string memory, uint, bool){
+    function ReadHospital(address _address) 
+    public onlyOracle (oracleAddress,owner) view returns(string memory, string memory, string memory, bool){
         uint index = FindHospital(_address);
-        return (hospitalCenters[index].name,hospitalCenters[index].city,hospitalCenters[index].state,
+        return (hospitalCenters[index].name,hospitalCenters[index].state,
         hospitalCenters[index].postalCode,hospitalCenters[index].permission);
     }
 
     // *********************************** DOCTORS ***********************************
 
     // Add new health professional
-    function AddDoctor(address idDoctor, string memory _nameDoctor, address _hospital) public {
+    function AddDoctor(address idDoctor, string memory _nameDoctor, address _hospital) 
+    public onlyOracle (oracleAddress,owner){
         FindHospital(_hospital);
         doctorsHospital.push(Doctor(idDoctor, _nameDoctor, _hospital, true));
         emit NewDoctor(idDoctor,_nameDoctor,_hospital,true);
@@ -149,19 +127,16 @@ contract EHealth {
         }
         revert('Doctor not found');
     }
-
     // Remove permission of the doctor
-    function RemoveDoctor(address _id) public {
+    function RemoveDoctor(address _id) 
+    public onlyOracle (oracleAddress,owner){
         uint index = FindDoctor(_id);
         doctorsHospital[index].permission=false;
-        emit DeleteDoctor(doctorsHospital[index].idDoctor,
-        doctorsHospital[index].nameDoctor,
-        doctorsHospital[index].hospital,
-        doctorsHospital[index].permission);
+        emit DeleteDoctor(doctorsHospital[index].permission);
     }
-
     // Update doctor data
-    function UpdateDoctor(address _id, string memory _nameDoctor, address _hospital ,bool _permission) public {
+    function UpdateDoctor(address _id, string memory _nameDoctor, address _hospital ,bool _permission) 
+    public onlyOracle (oracleAddress,owner){
         uint index = FindDoctor(_id);
         doctorsHospital[index].nameDoctor=_nameDoctor;
         doctorsHospital[index].hospital=_hospital;
@@ -172,29 +147,20 @@ contract EHealth {
         doctorsHospital[index].permission
         );
     }
-
     // Read doctor data
-    function ReadDoctor(address _id)public view returns(string memory, address, bool){
+    function ReadDoctor(address _id)
+    public onlyOracle (oracleAddress,owner) view returns(string memory, address, bool){
         uint index = FindDoctor(_id);
         return (doctorsHospital[index].nameDoctor,doctorsHospital[index].hospital,doctorsHospital[index].permission);
     }
-
-    // Number of doctors
-    function Length_Doctors() public view returns (uint256){
-        return hospitalCenters.length;
-    }
-
      // All Doctors 
-    function getAllDoctors() public view returns (Doctor[] memory){
-        Doctor[] memory ret = new Doctor[](doctorsHospital.length);
-        for (uint i = 0; i< doctorsHospital.length; i++) {
-            ret[i]=doctorsHospital[i];
-        }
-        return ret;
+    function getAllDoctors() 
+    public onlyOracle (oracleAddress,owner) view returns (Doctor[] memory){
+        return doctorsHospital;
     }
-
     // All Doctors in a hospital
-    function getAllDoctorsInHospital(address _address) public view returns (Doctor[] memory){
+    function getAllDoctorsInHospital(address _address) 
+    public onlyOracle (oracleAddress,owner) view returns (Doctor[] memory){
         Doctor[] memory ret = new Doctor[](doctorsHospital.length);
         for (uint i = 0; i< doctorsHospital.length; i++) {
             if(_address == doctorsHospital[i].hospital){
@@ -207,15 +173,15 @@ contract EHealth {
     // *********************************** SENSORS ***********************************
 
     // Add new sensor in the system 
-    function AddSensor(string memory _mac, string memory _patientName, address _idDoctor, bool _active) public {
+    function AddSensor(string memory _mac, string memory _patientName, address _idDoctor, bool _active) 
+    public onlyDoctor (_idDoctor, oracleAddress,owner){
         FindDoctor(_idDoctor);
         uint[] memory _data;
         sensorsDoctor.push(Sensor(nextIdSensor,_mac, _patientName, _idDoctor, _active, _data));
         emit NewSensor(nextIdSensor,_mac, _patientName, _idDoctor, _active, _data);
         nextIdSensor++;
     }
-
-     // Find sensor 
+    // Find sensor 
     function FindSensor(uint _idSensor) internal view returns(uint){
         for(uint i = 0 ; i < sensorsDoctor.length ; i++){
             if(sensorsDoctor[i].idSensor==_idSensor){
@@ -224,20 +190,16 @@ contract EHealth {
         }
         revert('Sensor not found');
     }
-
     // Remove active sensor
-    function RemoveSensor(uint _id) public {
+    function RemoveSensor(uint _id, address _idDoctor) 
+    public onlyDoctor (_idDoctor, oracleAddress,owner){
         uint index = FindSensor(_id);
         sensorsDoctor[index].active=false;
-        emit DeleteSensor(sensorsDoctor[index].idSensor,
-        sensorsDoctor[index].MAC,
-        sensorsDoctor[index].patientName,
-        sensorsDoctor[index].idDoctor,
-        sensorsDoctor[index].active);
+        emit DeleteSensor(sensorsDoctor[index].active);
     }
-
     // Update sensor data
-    function UpdateSensor(uint _id, string memory _mac, string memory _patientName,address _idDoctor, bool _active ) public {
+    function UpdateSensor(uint _id, string memory _mac, string memory _patientName,address _idDoctor, bool _active ) 
+    public onlyDoctor (_idDoctor, oracleAddress,owner){
         uint index = FindSensor(_id);
         sensorsDoctor[index].MAC=_mac;
         sensorsDoctor[index].patientName=_patientName;
@@ -249,29 +211,20 @@ contract EHealth {
         sensorsDoctor[index].idDoctor,
         sensorsDoctor[index].active);
     }
-
     // Read sensor data
-    function ReadSensor(uint _id)public view returns(string memory,string memory, address, bool){
+    function ReadSensor(uint _id)
+    public onlyOracle (oracleAddress,owner) view returns(string memory,string memory, address, bool){
         uint index = FindSensor(_id);
         return (sensorsDoctor[index].MAC,sensorsDoctor[index].patientName,sensorsDoctor[index].idDoctor,sensorsDoctor[index].active);
     }
-
-    // Number of sensors
-    function Length_sensors() public view returns (uint256){
-        return sensorsDoctor.length;
-    }
-
      // All Sensors 
-    function getAllSensors() public view returns (Sensor[] memory){
-        Sensor[] memory ret = new Sensor[](sensorsDoctor.length);
-        for (uint i = 0; i< sensorsDoctor.length; i++) {
-            ret[i]=sensorsDoctor[i];
-        }
-        return ret;
+    function getAllSensors() 
+    public onlyOracle (oracleAddress,owner) view returns (Sensor[] memory){
+        return sensorsDoctor;
     }
-
     // All medical sensors for a doctor
-    function getAllSensorOfDoctor (address _idDoctor) public view returns (Sensor[] memory){
+    function getAllSensorOfDoctor (address _idDoctor) 
+    public onlyDoctor (_idDoctor, oracleAddress,owner) view returns (Sensor[] memory){
         Sensor[] memory ret = new Sensor[](sensorsDoctor.length);
         for (uint i = 0; i< sensorsDoctor.length; i++) {
             if(_idDoctor == sensorsDoctor[i].idDoctor){
@@ -280,17 +233,27 @@ contract EHealth {
         }
         return ret;
     }
-
     // Sensor sending data
-    function sendData(uint _idSensor, uint[] memory _data) public{
+    function sendData(uint _idSensor, uint[] memory _data) 
+    public onlyOracle(oracleAddress,owner){
         uint index = FindSensor(_idSensor);
         sensorsDoctor[index].data=_data;
     }
-
     // View data for sensor
-    function viewSensorData(uint _idSensor) public view returns(uint[] memory){
+    function viewSensorData(uint _idSensor, address _idDoctor) 
+    public onlyDoctor (_idDoctor, oracleAddress,owner) view returns(uint[] memory){
         uint index = FindSensor(_idSensor);
         return sensorsDoctor[index].data;
     } 
+    // Modifiers
+    modifier onlyOracle(address _oracle, address _owner){
+        require((oracleAddress == _oracle) || (owner == _owner), 
+        "No privileges");
+        _;
+    }
+    modifier onlyDoctor(address _idDoctor,address _oracle, address _owner){
+        require((FindDoctor(_idDoctor) < 0) || (oracleAddress == _oracle) || (owner == _owner),"No privileges");
+        _;
+    }
 
 }
